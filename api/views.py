13c97509ed0777e5f2d40a20d0ce568bfb8d2862
Aaron Ayerdis <ayerdis.espinoza.aa@gmail.com>
2022-05-03 15:20:43 -0600

    6. Generating serializers for Room model class

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
 
 # Create your views here.
-def main(request):
-    return HttpResponse("Testing")
+
+# API view
+# It'll allows to see all of the different rooms but
+# actually create a room
+class RoomView(generics.CreateAPIView):
